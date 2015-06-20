f0881a0260d9fd23ab322cdf505014dcb1d47812
oroborusm <juan.bustamante.dg@gmail.com>
2015-06-20 19:25:31 -0300

    cambios finales

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,4 +5,26 @@
 	$('.carousel').carousel({
 		controls:true
 	});
+
+
+	$('.caract a').click(function (e) {
+	  e.preventDefault()
+	  $(this).tab('show')
+	})  
+	    
+	$('a.scroll').on('click', function (e) {
+		var href = $(this).attr('href');
+		$('html, body').animate({
+			scrollTop: $(href).offset().top
+		}, 'slow');
+		e.preventDefault();
+	});
+
+	$(".scrollchico").click(function() {
+
+		$('html, body').animate({
+			scrollTop: $(".objetivo").offset().top
+		},1000);
+	});
+
 })();
